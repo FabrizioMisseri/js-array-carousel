ad1d26784251ddb8e9d981590dfe20b993be889a
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-12 21:56:29 +0200

    add carousel-sidebar bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // **Consegna:**
 // Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+// **MILESTONE 1**
+// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
 const imgs = [
     "img/01.jpg",
     "img/02.jpg",
@@ -7,51 +9,66 @@ const imgs = [
     "img/04.jpg",
     "img/05.jpg"
 ];
-
-const itemContainer = document.querySelector(".item-container");
-// **MILESTONE 1**
-// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
 // **MILESTONE 2**
 // Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+const main = document.querySelector("main");
+
 for (let i = 0; i < imgs.length; i++) {
-    const thisImg = imgs[i];
-    const element = `
-    <div class="item">
-        <img src="${thisImg}">
-    </div>
-    `;
-    itemContainer.innerHTML += element;
-}
+    let thisImg = imgs[i];
+    let element = `<div class="item hidden"> <img src="${thisImg}"> </div>`;
+    main.innerHTML += element
+};
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
 const items = document.getElementsByClassName("item");
-let sliderPosition = 0;
-items[sliderPosition].classList.add("active");
+const darkVeil = document.getElementsByClassName("dark-veil");
+const card = document.getElementsByClassName("card");
+let itemSelector = 0;
+items[itemSelector].classList.remove("hidden");
+darkVeil[itemSelector].classList.add("hidden");
+card[itemSelector].classList.add("white-border");
+
+// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 // **MILESTONE 3**
 // Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
 const upBtn = document.querySelector(".up-arrow");
 const downBtn = document.querySelector(".down-arrow");
 
-upBtn.addEventListener ("click", function() {
-    if (sliderPosition < (items.length - 1)) {
-        items[sliderPosition].classList.remove("active");
-        sliderPosition++
-        items[sliderPosition].classList.add("active");
+upBtn.addEventListener("click", function() {
+    if (itemSelector < (items.length - 1)) {
+        items[itemSelector].classList.add("hidden");
+        darkVeil[itemSelector].classList.remove("hidden");
+        card[itemSelector].classList.remove("white-border");
+
+        itemSelector++
+
+        items[itemSelector].classList.remove("hidden");
+        darkVeil[itemSelector].classList.add("hidden");
+        card[itemSelector].classList.add("white-border");
+
+        downBtn.classList.remove("hidden");
     }
-    if (sliderPosition === (items.length - 1)) {
+    if (itemSelector == (items.length - 1)) {
         upBtn.classList.add("hidden");
     }
-    downBtn.classList.remove("hidden");
 })
 
-downBtn.addEventListener ("click", function() {
-    if (sliderPosition > 0) {
-        items[sliderPosition].classList.remove("active");
-        sliderPosition--
-        items[sliderPosition].classList.add("active");
+downBtn.addEventListener("click", function() {
+    if (itemSelector > 0) {
+        items[itemSelector].classList.add("hidden");
+        darkVeil[itemSelector].classList.remove("hidden");
+        card[itemSelector].classList.remove("white-border");
+
+        itemSelector--
+
+        items[itemSelector].classList.remove("hidden");
+        darkVeil[itemSelector].classList.add("hidden");
+        card[itemSelector].classList.add("white-border");
+
+        upBtn.classList.remove("hidden");
     }
-    if (sliderPosition === 0) {
+    if (itemSelector === 0) {
         downBtn.classList.add("hidden");
     }
-    upBtn.classList.remove("hidden");
 })
